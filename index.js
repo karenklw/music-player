5678f1c92ab59b77a8163a6318abea7603200769
Karen Wang <kw336@duke.edu>
2023-06-21 20:52:57 -0400

    fixed bugs and added event listeners

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const image = document.getElementById('cover'),
 title = document.getElementById('music-title'),
 artist = document.getElementById('music-artist'),
-currentTimeEL = document.getElementById('current-time'),
-durationEL = document.getElementById('duration'),
+currentTimeEl = document.getElementById('current-time'),
+durationEl = document.getElementById('duration'),
 progress = document.getElementById('progress'),
 playerProgress = document.getElementById('player-progress'),
 prevBtn = document.getElementById('prev'),
@@ -11,3 +11,86 @@ playBtn = document.getElementById('play'),
 background = document.getElementById('bg-img');
 
 const music = new Audio();
+
+const songs = [
+    {
+        path: 'assets/cupid.mp3',
+        displayName: 'Cupid (Twin Ver.)',
+        cover: 'assets/bg.jpg', // FIXME: replace img
+        artist: 'FIFTY FIFTY',
+    },
+    {
+        path: 'assets/leftright.mp3',
+        displayName: 'Left Right Remixx',
+        cover: 'assets/bg.jpg', // FIXME: replace img
+        artist: 'XG, Jakops',
+    }
+];
+
+let musicIndex = 0;
+let isPlaying = false;
+
+function togglePlay(){
+    if (isPlaying){
+        pauseMusic();
+    }
+    else{
+        playMusic();
+    }
+}
+
+function playMusic(){
+    isPlaying = true;
+
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+function pauseMusic(){
+    isPlaying = false;
+
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+function loadMusic(song){
+    music.src = song.path;
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    image.src = song.cover;
+    background.src = song.cover;
+}
+
+function changeMusic(direction){
+    musicIndex = (musicIndex + direction + songs.length) % songs.length;
+    loadMusic(songs[musicIndex]);
+    playMusic();
+}
+
+function updateProgressBar(){
+    const {duration, currentTime} = music;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
+    durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
+    currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
+
+}
+
+function setProgressBar(e){
+    const width = playerProgress.clientWidth;
+    const clickX = e.offsetX;
+    music.currentTime = (clickX / width) * music.duration;
+}
+
+playBtn.addEventListener('click', togglePlay);
+prevBtn.addEventListener('click', () => changeMusic(-1));
+nextBtn.addEventListener('click', () => changeMusic(1));
+music.addEventListener('ended', () => changeMusic(1));
+music.addEventListener('timeupdate', updateProgressBar);
+playerProgress.addEventListener('click', setProgressBar);
+
+loadMusic(songs[musicIndex]);
